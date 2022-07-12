8bc9b254534513aef1807f8b92af782e3919372c
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-07-12 11:45:05 -0500

    fixed some minor errors

--- pages/mortgageCalc.js
+++ pages/mortgageCalc.js
@@ -17,7 +17,7 @@ const mortgageCalc = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Mortgage App</h2>
+          <h2 className="py-2">Mortgage Calc</h2>
           <h3>Angular / TailwindCSS / PrimeNG</h3>
         </div>
       </div>
--- pages/pdfViewer.js
+++ pages/pdfViewer.js
@@ -17,7 +17,7 @@ const pdfViewer = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Mortgage App</h2>
+          <h2 className="py-2">PDF Viewer</h2>
           <h3>Angular / PDFTron / Firebase</h3>
         </div>
       </div>
