6a133145bf8c2e773772bb00c8f4696069940c03
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-07-12 11:41:57 -0500

    fixed some errors in airbnb section

--- pages/airbnb.js
+++ pages/airbnb.js
@@ -17,7 +17,7 @@ const airbnb = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Mortgage App</h2>
+          <h2 className="py-2">AirBnB</h2>
           <h3>React / Next / TailwindCSS</h3>
         </div>
       </div>
@@ -56,7 +56,7 @@ const airbnb = () => {
                 <RiRadioButtonFill className="pr-1" /> React
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Next
+                <RiRadioButtonFill className="pr-1" /> NextJs
               </p>
               <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> Geolib
